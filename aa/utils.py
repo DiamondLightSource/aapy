911ee489953e7fae86d92046fe269c8a1cca7a03
Will Rogers <wahrogers@gmail.com>
2017-08-30 08:23:13 +0100

    Tests for utils.concatenate().

--- aa/utils.py
+++ aa/utils.py
@@ -6,15 +6,16 @@ import pytz
 
 EPOCH = pytz.UTC.localize(datetime(1970, 1, 1))
 ArchiveData = collections.namedtuple('ArchiveData', ('pv', 'values', 'timestamps', 'severities'))
+DIFFERENT_PV_ERROR = 'All concatenated ArchiveData objects must have the same PV name'
 
 
 def concatenate(data_seq):
     pv = data_seq[0].pv
     for data in data_seq:
-        assert data.pv == pv
-    values = numpy.concatenate(data.values for data in data_seq)
-    timestamps = numpy.concatenate(data.timestamps for data in data_seq)
-    severities = numpy.concatenate(data.severities for data in data_seq)
+        assert data.pv == pv, DIFFERENT_PV_ERROR
+    values = numpy.concatenate([data.values for data in data_seq])
+    timestamps = numpy.concatenate([data.timestamps for data in data_seq])
+    severities = numpy.concatenate([data.severities for data in data_seq])
     return ArchiveData(pv, values, timestamps, severities)
 
 
