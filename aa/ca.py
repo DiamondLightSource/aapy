d9da181dbbfc4f4e6e231d975f8b44422829114d
Will Rogers <wahrogers@gmail.com>
2017-08-31 16:15:56 +0100

    Use multiple requests if necessary in the CaFetcher.

--- aa/ca.py
+++ aa/ca.py
@@ -15,39 +15,48 @@ class CaFetcher(Fetcher):
         self._url = url
         self._proxy = ServerProxy(url)
 
-    def get_values(self, pv, start, end=None, count=None):
-        count = 10000 if count is None else count
-        start_secs = utils.datetime_to_epoch(start)
-        end_secs = utils.datetime_to_epoch(end)
-        data = self._proxy.archiver.values(1, [pv], start_secs, 0, end_secs, 0, count, 0)
-        event_count = len(data[0]['values'])
-        wf_length = len(data[0]['values'][0]['value'])
+    def _process_raw_data(self, data, pv):
+        event_count = len(data)
+        wf_length = len(data[0]['value'])
         values = numpy.zeros((event_count, wf_length))
         timestamps = numpy.zeros((event_count,))
         sevs = numpy.zeros((event_count,))
-        count = 0
-        previous_val = {}
-        for val in data[0]['values']:
-            if val == previous_val:
-                log.debug('Continuing on duplicate event %s', val)
-                continue
-            previous_val = val
-
+        for count, val in enumerate(data):
             timestamp = val['secs'] + 1e-9 * val['nano']
-            if timestamp < start_secs:
-                continue
-            if val['sevr'] > 3:
-                log.debug('Discarding error event %s %s',
-                          utils.epoch_to_datetime(timestamp), val)
-                continue
             timestamps[count] = timestamp
             values[count] = val['value']
             sevs[count] = val['sevr']
-            count += 1
+        return aa.ArchiveData(pv, numpy.squeeze(values), timestamps, sevs)
 
-        return aa.ArchiveData(pv, numpy.squeeze(values[:count]),
-                              timestamps[:count], sevs[:count])
-
+    def get_values(self, pv, start, end=None, count=None):
+        # Make count a large number if not specified to ensure we get all
+        # data.
+        count = 2**31 if count is None else count
+        requested = 10000 if count > 10000 else count
+        start_secs = utils.datetime_to_epoch(start)
+        end_secs = utils.datetime_to_epoch(end)
+        response = self._proxy.archiver.values(1, [pv], start_secs, 0,
+                                               end_secs, 0, requested, 0)
+        done = len(response[0]['values']) < requested
+        data = self._process_raw_data(response[0]['values'], pv)
+        while done is not True and len(data.values) < count:
+            log.warn('{} samples requested; {} fetched so far.'.format(count,
+                                                                       len(data.values)))
+            # The first two samples will be the last from the previous request
+            # and the one before that.
+            requested = count - len(data.values) + 2
+            log.warn('Making additional request for {} samples.'.format(requested))
+            start_secs = int(data.timestamps[-1])
+            start_nanos = int(1e9 * (data.timestamps[-1] % 1))
+            response = self._proxy.archiver.values(1, [pv],
+                                                   start_secs, start_nanos,
+                                                   end_secs, 0,
+                                                   requested, 0)
+            done = len(response) < requested
+            # Discard the two samples we already have.
+            new_data = self._process_raw_data(response[0]['values'][2:], pv)
+            data = utils.concatenate((data, new_data))
+        return data
 
     def get_value_at(self, pv, instant):
-        return self._get_values(pv, instant, instant, 1)
+        return self.get_values(pv, instant, instant, 1)
