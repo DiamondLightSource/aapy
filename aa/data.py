2beb89135e4989be9924cd31e69bbb99659e3627
Will Rogers <wahrogers@gmail.com>
2017-09-26 16:10:19 +0100

    Allow identical timestamps in ArchiveData.

--- aa/data.py
+++ aa/data.py
@@ -83,7 +83,7 @@ class ArchiveData(object):
     def append(self, other):
         assert other.pv == self.pv, DIFFERENT_PV_ERROR
         timestamps = numpy.concatenate([self.timestamps, other.timestamps])
-        assert numpy.all(numpy.diff(timestamps) > 0), TIMESTAMP_ERROR
+        assert numpy.all(numpy.diff(timestamps) >= 0), TIMESTAMP_ERROR
         self._values = numpy.concatenate([self.values, other.values])
         self._timestamps = timestamps
         self._severities = numpy.concatenate([self.severities, other.severities])
