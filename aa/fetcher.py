6734210ade87997e621bcf2cf8be746790319491
Will Rogers <wahrogers@gmail.com>
2017-09-29 09:55:59 +0100

    Minor rearrangement of url handling.
    Put logic in the utils class so it can be mocked if necessary.

--- aa/fetcher.py
+++ aa/fetcher.py
@@ -1,4 +1,4 @@
-from .utils import quote, urlopen
+from .utils import urlquote, urlget
 import pytz
 from datetime import datetime
 
@@ -33,17 +33,16 @@ class AaFetcher(Fetcher):
         return dt.strftime('%Y-%m-%dT%H:%M:%SZ')
 
     def _construct_url(self, pv, start, end):
-        encoded_pv = quote(pv)
-        encoded_start = quote(self._format_datetime(start))
-        encoded_end = quote(self._format_datetime(end))
+        encoded_pv = urlquote(pv)
+        encoded_start = urlquote(self._format_datetime(start))
+        encoded_end = urlquote(self._format_datetime(end))
         suffix = '?pv={}&from={}&to={}'.format(encoded_pv,
                 encoded_start, encoded_end)
         return '{}{}'.format(self._url, suffix)
 
     def _fetch_data(self, pv, start, end):
         url = self._construct_url(pv, start, end)
-        urlinfo = urlopen(url)
-        return urlinfo.read()
+        return urlget(url)
 
     def _get_values(self, pv, start, end, count):
         raw_data = self._fetch_data(pv, start, end)
