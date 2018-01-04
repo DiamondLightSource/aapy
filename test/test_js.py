3802a9a1a5c1690e7fa9b480457409b8c386bd21
Will Rogers <wahrogers@gmail.com>
2018-01-04 15:11:52 +0000

    Add test for waveforms using JsonFetcher.
    Move json data to separate files.

--- test/test_js.py
+++ test/test_js.py
@@ -3,26 +3,27 @@ from datetime import datetime
 import numpy
 import mock
 import pytest
+import os
 
-
-JSON_DEMO = """
-[{
-  "meta": {},
-  "data":
-  [
-    {
-      "secs": 1502963093,
-      "nanos": 123,
-      "val": 1.23,
-      "severity": 1
-    }
-  ]
-}]
-"""
 
 EARLY_DATE = datetime(2001, 1, 1, 1, 1)
 LATE_DATE = datetime(2010, 2, 3, 4, 5)
 EMPTY_ARRAY = numpy.array((0,))
+
+
+def load_from_file(filename):
+    """
+    Load the contents of a file in the data directory.
+    Args:
+        filename: name of file to load
+
+    Returns:
+        contents of file as a string
+
+    """
+    filepath = os.path.join(os.path.dirname(__file__), 'data', filename)
+    with open(filepath) as f:
+        return f.read()
 
 
 @pytest.fixture
@@ -40,10 +41,18 @@ def test_JsonFetcher_decodes_empty_json_correctly(dummy_pv, empty_data, json_fet
     assert aa_data == empty_data
 
 
-def test_JsonFetcher_decodes_json_correctly(dummy_pv, json_fetcher):
-    json_fetcher._fetch_data = mock.MagicMock(return_value=JSON_DEMO)
+def test_JsonFetcher_decodes_single_event_correctly(dummy_pv, json_fetcher):
+    event_json = load_from_file('event.json')
+    json_fetcher._fetch_data = mock.MagicMock(return_value=event_json)
     aa_data = json_fetcher.get_values(dummy_pv, EARLY_DATE, LATE_DATE)
     assert aa_data.pv == dummy_pv
     assert aa_data.values[0] == 1.23
     assert aa_data.timestamps[0] == 1502963093.000000123
     assert aa_data.severities[0] == 1
+
+
+def test_JsonFetcher_decodes_waveform_events_correctly(dummy_pv, json_fetcher, data_2d_2_events):
+    waveform_json = load_from_file('waveform.json')
+    json_fetcher._fetch_data = mock.MagicMock(return_value=waveform_json)
+    aa_data = json_fetcher.get_values(dummy_pv, EARLY_DATE, LATE_DATE)
+    assert aa_data == data_2d_2_events
