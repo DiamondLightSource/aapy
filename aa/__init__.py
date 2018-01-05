01f5d6a409fea41430671412eaf29f3d1c20b8a0
Will Rogers <wahrogers@gmail.com>
2018-01-05 15:44:34 +0000

    Add docstrings.

--- aa/__init__.py
+++ aa/__init__.py
@@ -1,3 +1,6 @@
+"""
+Python client to the EPICS Archiver Appliance.
+"""
 from . import js
 from .data import ArchiveData, ArchiveEvent
 import logging
@@ -13,10 +16,36 @@ PORT = 8080
 
 
 def get_value_at(pv, date):
+    """Retrieve the one event preceding the specified datetime.
+
+    Args:
+        pv: PV to query
+        date: datetime before which to find the event
+
+    Returns:
+        ArchiveEvent object representing one event
+
+    """
     fetcher = js.JsonFetcher(URL, PORT)
     return fetcher.get_event_at(pv, date)
 
 
 def get_values(pv, start_date, end_date=None, count=None):
+    """Retrieve all values between the start and end datetimes.
+    If no end datetime is specified, return all events to the current time.
+    If count is specified, limit to the first count events.
+
+    The event preceding the first datetime will also be included.
+
+    Args:
+        pv: PV to query
+        start_date: start of period for which to retrieve events
+        end_date: end of period for which to retrieve events
+        count: restrict number of events to count
+
+    Returns:
+        ArchiveData object representing all events in specified period
+
+    """
     fetcher = js.JsonFetcher(URL, PORT)
     return fetcher.get_values(pv, start_date, end_date, count)
