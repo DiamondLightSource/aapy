26853ed5bc0134c5ef6ac5b314e9a06eaf002a5d
Will Rogers <wahrogers@gmail.com>
2018-01-05 13:35:27 +0000

    Add Apache Licence 2.0.

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from setuptools import setup
 
 setup(
         name='aapy',
-        description='Python client for the EPICS Archiver Appliance'
+        description='Python client for the EPICS Archiver Appliance',
+        license='Apache License 2.0'
      )
