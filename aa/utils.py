64bded47f50ed6d4876bc425b28782538b16000e
Will Rogers <wahrogers@gmail.com>
2018-01-31 14:45:48 +0000

    Add debug logging to urlget function.

--- aa/utils.py
+++ aa/utils.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 from datetime import datetime
+import logging as log
 import pytz
 # Manage differences between Python 2 and Python 3.
 try:
@@ -22,6 +23,7 @@ EPOCH = pytz.UTC.localize(datetime(1970, 1, 1))
 
 
 def urlget(url):
+    log.debug('Fetching url {}'.format(url))
     urlinfo = urlopen(url)
     return urlinfo.read()
 
