aa89556b3db77033f6ec599543273eedb4386fdb
Will Rogers <wahrogers@gmail.com>
2018-03-16 12:04:47 +0000

    Only set up logging if requested.

--- aa/__init__.py
+++ aa/__init__.py
@@ -4,6 +4,15 @@ Python client to the EPICS Archiver Appliance.
 import logging
 
 
-LOG_FORMAT = '%(levelname)s:  %(message)s'
-LOG_LEVEL = logging.INFO
-logging.basicConfig(format=LOG_FORMAT, level=LOG_LEVEL)
+LOG_FORMAT = '%(asctime)s %(levelname)-8s %(message)s'
+LOG_LEVEL = logging.DEBUG
+
+
+def set_up_logging(format=LOG_FORMAT, level=LOG_LEVEL):
+    logging.basicConfig(
+        format=format, level=level, datefmt='%Y-%m-%d %I:%M:%S'
+    )
+
+
+SCAN = 'SCAN'
+MONITOR = 'MONITOR'
