7f46e65718b2cbc9672f0e512bfc9d04a1e902dd
Will Rogers <wahrogers@gmail.com>
2018-03-19 16:30:02 +0000

    Bump version to 0.2.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
       name='aapy',
-      version='0.0.1',
+      version='0.2',
       description='Python client for the EPICS Archiver Appliance',
       license='Apache License 2.0',
       packages=['aa'],
