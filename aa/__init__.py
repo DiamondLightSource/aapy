38b9b6b1e1d7046029bf11a28f912f0baf027fe7
Will Rogers <wahrogers@gmail.com>
2018-04-17 09:57:04 +0100

    Rearrange module.

--- aa/__init__.py
+++ aa/__init__.py
@@ -5,6 +5,13 @@ import logging
 import tzlocal
 
 
+SCAN = 'SCAN'
+MONITOR = 'MONITOR'
+
+# Make local timezone easy to get hold of.
+LOCALTZ = tzlocal.get_localzone()
+
+# Logging utilities
 LOG_FORMAT = '%(asctime)s %(levelname)-8s %(message)s'
 LOG_LEVEL = logging.DEBUG
 
@@ -13,10 +20,3 @@ def set_up_logging(format=LOG_FORMAT, level=LOG_LEVEL):
     logging.basicConfig(
         format=format, level=level, datefmt='%Y-%m-%d %I:%M:%S'
     )
-
-
-SCAN = 'SCAN'
-MONITOR = 'MONITOR'
-
-# Make local timezone easy to get hold of.
-LOCALTZ = tzlocal.get_localzone()
