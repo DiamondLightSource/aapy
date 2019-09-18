7cffad90ca44d1ba5834076df2b97c784d98d8ad
Will Rogers <will.rogers@diamond.ac.uk>
2019-09-18 09:05:15 +0100

    Update version number to 0.6.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 
 setup(
       name='aapy',
-      version='0.4',
+      version='0.6',
       description='Python client for the EPICS Archiver Appliance',
       license='Apache License 2.0',
       packages=['aa'],
       install_requires=[
-          'numpy',
+          'numpy<=1.16',
           'protobuf',
           'pytz',
           'requests',
