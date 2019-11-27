50bb26e2f799fa1571ba2ca2ecf3c58c8f87d7db
Andy Wilson <andrew.wilson@diamond.ac.uk>
2019-11-27 10:33:45 +0000

    Trivial refactor of test file

--- test/pb_tools/test_validation.py
+++ test/pb_tools/test_validation.py
@@ -2,6 +2,10 @@ from pytest import fixture
 from aa import epics_event_pb2 as ee
 from aa.pb_tools import validation
 
+
+#######################################################
+# Fixtures
+#######################################################
 @fixture()
 def test_events():
     events = [
@@ -23,6 +27,8 @@ def payload_info():
     )
 
 
+#######################################################
+
 def test_basic_data_checks(test_events, payload_info):
 
     # Expect:
@@ -30,7 +36,6 @@ def test_basic_data_checks(test_events, payload_info):
     # No value at 1
     # Timestamp duplicated at 2
     # Event out of order at 3
-
     expected_errors = [
         (0, validation.PbError.EVENT_NOT_DECODED),
         (1, validation.PbError.EVENT_MISSING_VALUE),
@@ -38,17 +43,11 @@ def test_basic_data_checks(test_events, payload_info):
         (3, validation.PbError.EVENT_OUT_OF_ORDER),
         (4, validation.PbError.EVENT_DUPLICATED),
     ]
-
-
-
     result = validation.basic_data_checks(payload_info, test_events)
-
     assert result == expected_errors
 
     # With no header attached
-
     result = validation.basic_data_checks(None, test_events)
-
     assert result == [(None, validation.PbError.HEADER_NOT_DECODED)]
 
 
@@ -60,12 +59,15 @@ def test_basic_data_checks_with_lazy_returns_after_first(test_events,
     expected_errors = [
         (0, validation.PbError.EVENT_NOT_DECODED),
     ]
-
     assert result == expected_errors
 
 
 def test_basic_data_checks_with_only_check_returns_only_requested(test_events,
                                                                   payload_info):
+    """
+    Test different variations of "only_check" mask to make sure we
+    get the right subset of error sreported
+    """
 
     mask = validation.PbError.ALL
 
