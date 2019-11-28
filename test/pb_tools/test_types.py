cf513dfbe9f36844b3d08dc2610d221b1eb01a99
Andy Wilson <andrew.wilson@diamond.ac.uk>
2019-11-28 11:01:50 +0000

    Further tests for types

--- test/pb_tools/test_types.py
+++ test/pb_tools/test_types.py
@@ -10,9 +10,26 @@ from aa import epics_event_pb2 as ee
     (dbr.ca_int,   dbr.DBR_LONG,   ee.ScalarInt),
     (dbr.ca_float, dbr.DBR_FLOAT,  ee.ScalarFloat),
     (dbr.ca_float, dbr.DBR_DOUBLE, ee.ScalarDouble),
+    (dbr.ca_int,   dbr.DBR_CHAR,   ee.ScalarByte),
 ])
-def test_ca_to_pb_type_gives_correct_output(ca_type, dbr_type, pb_type):
+def test_ca_to_pb_type_gives_correct_scalar_output(ca_type, dbr_type, pb_type):
     test_ca_obj = ca_type()
+    test_ca_obj.datatype = dbr_type
+
+    result = types.ca_to_pb_type(test_ca_obj)
+
+    assert result == pb_type
+
+@mark.parametrize("ca_type,dbr_type,pb_type",[
+    (dbr.ca_array, dbr.DBR_STRING, ee.VectorString),
+    (dbr.ca_array, dbr.DBR_SHORT,  ee.VectorShort),
+    (dbr.ca_array, dbr.DBR_LONG,   ee.VectorInt),
+    (dbr.ca_array, dbr.DBR_FLOAT,  ee.VectorFloat),
+    (dbr.ca_array, dbr.DBR_DOUBLE, ee.VectorDouble),
+    (dbr.ca_array, dbr.DBR_CHAR,   ee.VectorChar),
+])
+def test_ca_to_pb_type_gives_correct_vector_output(ca_type, dbr_type, pb_type):
+    test_ca_obj = ca_type([0,1,2,3])
     test_ca_obj.datatype = dbr_type
 
     result = types.ca_to_pb_type(test_ca_obj)
