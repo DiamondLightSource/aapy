0b92c88b0888ec53d3b99e32d03a37fa7400fa1d
Andy Wilson <andrew.wilson@diamond.ac.uk>
2019-11-28 10:56:16 +0000

    More tests for type mappings

--- test/pb_tools/test_types.py
+++ test/pb_tools/test_types.py
@@ -1,12 +1,20 @@
+from pytest import mark
 from cothread.catools import dbr
 
 from aa.pb_tools import types
 from aa import epics_event_pb2 as ee
 
-def test_ca_to_pb_type_gives_correct_output():
-    first_test = dbr.ca_str()
-    first_test.datatype = dbr.DBR_STRING
+@mark.parametrize("ca_type,dbr_type,pb_type",[
+    (dbr.ca_str,   dbr.DBR_STRING, ee.ScalarString),
+    (dbr.ca_int,   dbr.DBR_SHORT,  ee.ScalarShort),
+    (dbr.ca_int,   dbr.DBR_LONG,   ee.ScalarInt),
+    (dbr.ca_float, dbr.DBR_FLOAT,  ee.ScalarFloat),
+    (dbr.ca_float, dbr.DBR_DOUBLE, ee.ScalarDouble),
+])
+def test_ca_to_pb_type_gives_correct_output(ca_type, dbr_type, pb_type):
+    test_ca_obj = ca_type()
+    test_ca_obj.datatype = dbr_type
 
-    result = types.ca_to_pb_type(first_test)
+    result = types.ca_to_pb_type(test_ca_obj)
 
-    assert result == ee.ScalarString
+    assert result == pb_type
