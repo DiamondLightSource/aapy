c2b58ede0498826f14252da30f440b7aa8460f0b
Andy Wilson <andrew.wilson@diamond.ac.uk>
2021-06-25 15:45:35 +0100

    Copy Pipfile*, setup* from DLS branch

--- setup.py
+++ setup.py
@@ -1,17 +1,5 @@
 from setuptools import setup
 
 
-setup(
-      name='aapy',
-      version='0.8',
-      description='Python client for the EPICS Archiver Appliance',
-      license='Apache License 2.0',
-      packages=['aa'],
-      install_requires=[
-          'numpy<=1.16',
-          'protobuf',
-          'pytz',
-          'requests',
-          'tzlocal',
-      ]
-     )
+
+setup()
