082bf296aa9835365239ca34273520c074ca97a4
aawdls <aawdls@users.noreply.github.com>
2021-08-10 07:28:45 +0000

    deploy: ffd94895a342f5e446855cda8c7fb51697033790

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0+unknown',
+    VERSION: '0.11+5.gffd9489',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
